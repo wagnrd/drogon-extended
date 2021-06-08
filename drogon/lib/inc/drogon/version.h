d0ecaf9b412491cd9ff6419894e0e7d39b8cc929
Denis Wagner <denis.wagner@valtech-mobility.com>
2021-06-08 10:36:33 +0200

    moves exception mapper to correct folder

--- drogon/lib/inc/drogon/version.h
+++ drogon/lib/inc/drogon/version.h
@@ -2,4 +2,4 @@
 #define MINOR 5
 #define PATCH 1
 #define DROGON_VERSION "1.5.1"
-#define DROGON_VERSION_SHA1 "c2210859246cbb30c09cd4be8f87acbaa3b2f4f9"
+#define DROGON_VERSION_SHA1 "5c17fb29152c5031eab792ba893f7fe5fcda76a6"
