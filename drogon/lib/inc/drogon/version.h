22c50c99425486af944f0accc0f34f262ee12775
Denis Wagner <denis.wagner@valtech-mobility.com>
2021-06-08 10:21:12 +0200

    updates version

--- drogon/lib/inc/drogon/version.h
+++ drogon/lib/inc/drogon/version.h
@@ -2,4 +2,4 @@
 #define MINOR 5
 #define PATCH 1
 #define DROGON_VERSION "1.5.1"
-#define DROGON_VERSION_SHA1 "209e63ab0aeb5bcb91f26c4d07bec7dd2e131b02"
+#define DROGON_VERSION_SHA1 "8c68d4bf7eea6b21e54145414288a1bff215b696"
