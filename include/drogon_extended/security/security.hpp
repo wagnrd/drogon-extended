eb64d4fcb5160c18302f4bc349ddd1ac088824b2
Denis Wagner <denis.wagner@valtech-mobility.com>
2021-06-25 20:51:37 +0200

    adds security guard for jwt

--- include/drogon_extended/security/security.hpp
+++ include/drogon_extended/security/security.hpp
@@ -1,22 +1,59 @@
 #ifndef DROGON_EXTENDED_SECURITY_HPP
 #define DROGON_EXTENDED_SECURITY_HPP
 
+#include <jwt-cpp/jwt.h>
 #include <drogon/HttpResponse.h>
 #include <drogon/HttpRequest.h>
 
-#define MULTI_SECURITY_GUARD(authorizationConfig1, boolOperator, authorizationConfig2)          \
+void test()
+{
+    auto decodedJwtToken = jwt::decode("");
+    auto verifier = jwt::verify()
+            .allow_algorithm(jwt::algorithm::rs256{""})
+            .with_issuer("");
+    std::error_code error;
+    verifier.verify(decodedJwtToken, error);
+    if (error != jwt::error::rsa_error::ok)
+    {
+    }
+}
+
+#define API_KEY(apiKey) isUnauthorized = authorization != (apiKey);
+
+#define JWT_TOKEN(issuer, publicKey)                                                    \
+    auto tokenType = authorization.substr(0, 6);                                        \
+                                                                                        \
+    if (tokenType != "Bearer")                                                          \
+    {                                                                                   \
+        LOG_DEBUG << "Token type '" << tokenType << "' is not Bearer!";                 \
+                                                                                        \
+        auto response = ExceptionMapper::build_exception_response(                      \
+                                drogon::HttpStatusCode::k401Unauthorized,               \
+                                "Authorization failed",                                 \
+                                "Provided token type is invalid: '" + tokenType + "'"   \
+        );                                                                              \
+        callback(response);                                                             \
+        return;                                                                         \
+    }                                                                                   \
+                                                                                        \
+    auto jwtToken = authorization.substr(7);                                            \
+    auto decodedJwtToken = jwt::decode(jwtToken);                                       \
+                                                                                        \
+    auto verifier = jwt::verify()                                                       \
+            .allow_algorithm(jwt::algorithm::rs256{publicKey})                          \
+            .with_issuer(issuer);                                                       \
+    verifier.verify(decodedJwtToken);                                                   \
+                                                                                        \
+    if (jwt::error::rsa_error::ok
+
+#define SECURITY_GUARD(authorizationConfig)                                                     \
     auto authorization = request->getHeader("Authorization");                                   \
+    LOG_DEBUG << "Authorization: " << authorization;                                            \
+                                                                                                \
     bool isUnauthorized;                                                                        \
-    bool isUnauthorized1;                                                                       \
-    bool isUnauthorized2;                                                                       \
+    authorizationConfig                                                                         \
                                                                                                 \
-    authorizationConfig1                                                                        \
-    isUnauthorized1 = isUnauthorized;                                                           \
-                                                                                                \
-    authorizationConfig2                                                                        \
-    isUnauthorized2 = isUnauthorized;                                                           \
-                                                                                                \
-    if (isUnauthorized1 boolOperator isUnauthorized2)                                           \
+    if (isUnauthorized)                                                                         \
     {                                                                                           \
         auto response = ExceptionMapper::build_exception_response(                              \
                                 drogon::HttpStatusCode::k401Unauthorized,                       \
@@ -26,15 +63,5 @@
         callback(response);                                                                     \
         return;                                                                                 \
     }
-
-#define SECURITY_GUARD(authorizationConfig) MULTI_SECURITY_GUARD(authorizationConfig, AND, DUMMY_AUTHORIZATION(true))
-
-#define OR ||
-#define AND &&
-
-#define DUMMY_AUTHORIZATION(boolValue) isUnauthorized = boolValue;
-
-#define API_KEY(apiKey)                 \
-    isUnauthorized = authorization != apiKey; \
 
 #endif //DROGON_EXTENDED_SECURITY_HPP
