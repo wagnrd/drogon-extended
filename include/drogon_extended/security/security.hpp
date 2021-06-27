bab5a065bfe3f8e891ceaa0d2f6ad5b12f47a05a
wagnrd <denis@wagnrd.de>
2021-06-27 14:35:30 +0200

    fixes jwt token security guard

--- include/drogon_extended/security/security.hpp
+++ include/drogon_extended/security/security.hpp
@@ -5,46 +5,44 @@
 #include <drogon/HttpResponse.h>
 #include <drogon/HttpRequest.h>
 
-void test()
-{
-    auto decodedJwtToken = jwt::decode("");
-    auto verifier = jwt::verify()
-            .allow_algorithm(jwt::algorithm::rs256{""})
-            .with_issuer("");
-    std::error_code error;
-    verifier.verify(decodedJwtToken, error);
-    if (error != jwt::error::rsa_error::ok)
-    {
-    }
-}
-
 #define API_KEY(apiKey) isUnauthorized = authorization != (apiKey);
 
-#define JWT_TOKEN(issuer, publicKey)                                                    \
-    auto tokenType = authorization.substr(0, 6);                                        \
-                                                                                        \
-    if (tokenType != "Bearer")                                                          \
-    {                                                                                   \
-        LOG_DEBUG << "Token type '" << tokenType << "' is not Bearer!";                 \
-                                                                                        \
-        auto response = ExceptionMapper::build_exception_response(                      \
-                                drogon::HttpStatusCode::k401Unauthorized,               \
-                                "Authorization failed",                                 \
-                                "Provided token type is invalid: '" + tokenType + "'"   \
-        );                                                                              \
-        callback(response);                                                             \
-        return;                                                                         \
-    }                                                                                   \
-                                                                                        \
-    auto jwtToken = authorization.substr(7);                                            \
-    auto decodedJwtToken = jwt::decode(jwtToken);                                       \
-                                                                                        \
-    auto verifier = jwt::verify()                                                       \
-            .allow_algorithm(jwt::algorithm::rs256{publicKey})                          \
-            .with_issuer(issuer);                                                       \
-    verifier.verify(decodedJwtToken, error);                                            \
-    LOG_DEBUG << "Error: " << error;                                                    \
-    LOG_DEBUG << "RSA_OK: " << jwt::error::rsa_error::ok;
+#define JWT_TOKEN(issuer, publicKey)                                                            \
+    auto tokenType = authorization.substr(0, 6);                                                \
+                                                                                                \
+    if (tokenType != "Bearer")                                                                  \
+    {                                                                                           \
+        LOG_DEBUG << "Token type '" << tokenType << "' is not Bearer!";                         \
+                                                                                                \
+        auto response = ExceptionMapper::build_exception_response(                              \
+                                drogon::HttpStatusCode::k401Unauthorized,                       \
+                                "Authorization failed",                                         \
+                                "Provided token type is invalid: '" + tokenType + "'"           \
+        );                                                                                      \
+        callback(response);                                                                     \
+        return;                                                                                 \
+    }                                                                                           \
+                                                                                                \
+    auto jwtToken = authorization.substr(7);                                                    \
+    auto decodedJwtToken = jwt::decode(jwtToken);                                               \
+                                                                                                \
+    auto verifier = jwt::verify()                                                               \
+            .allow_algorithm(jwt::algorithm::rs256{publicKey});                                 \
+                                                                                                \
+    std::error_code error;                                                                      \
+    verifier.verify(decodedJwtToken, error);                                                    \
+                                                                                                \
+    if (error.value() != static_cast<int>(jwt::error::token_verification_error::ok))            \
+    {                                                                                           \
+        auto response = ExceptionMapper::build_exception_response(                              \
+                                drogon::HttpStatusCode::k401Unauthorized,                       \
+                                "Authorization failed",                                         \
+                                "Provided authorization is invalid: '" + authorization + "'"    \
+        );                                                                                      \
+                                                                                                \
+        callback(response);                                                                     \
+        return;                                                                                 \
+    }
 
 
 #define SECURITY_GUARD(authorizationConfig)                                                     \
