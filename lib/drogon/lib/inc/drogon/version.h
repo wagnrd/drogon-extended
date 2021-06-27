12827fe446cd5f71c31b5ee828673fa2b16a302b
wagnrd <denis@wagnrd.de>
2021-06-27 21:56:04 +0200

    adds draft client and controller routes

--- lib/drogon/lib/inc/drogon/version.h
+++ lib/drogon/lib/inc/drogon/version.h
@@ -2,4 +2,4 @@
 #define MINOR 6
 #define PATCH 0
 #define DROGON_VERSION "1.6.0"
-#define DROGON_VERSION_SHA1 "9bf1666d334afb2bb94f35bbfe9b62f220c2f563"
+#define DROGON_VERSION_SHA1 "6eedfe776d5a1fa3fa4cf98c73bba5c4032d6eaf"
