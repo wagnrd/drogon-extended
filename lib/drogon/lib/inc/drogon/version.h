f93db5d0b30007f52f5890cdf5da58076c28d585
wagnrd <denis@wagnrd.de>
2021-06-27 13:45:01 +0200

    fixes includes for new drogon_extended target

--- lib/drogon/lib/inc/drogon/version.h
+++ lib/drogon/lib/inc/drogon/version.h
@@ -2,4 +2,4 @@
 #define MINOR 6
 #define PATCH 0
 #define DROGON_VERSION "1.6.0"
-#define DROGON_VERSION_SHA1 "45e8bc0d1b09ee5bfdaf7c456eae13d4a610f919"
+#define DROGON_VERSION_SHA1 "9bf1666d334afb2bb94f35bbfe9b62f220c2f563"
